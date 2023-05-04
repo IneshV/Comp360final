e3cfe996682239bfea686d47a811e02b3cac2fa9
MilesM326 <132500239+MilesM326@users.noreply.github.com>
2023-05-04 18:01:51 -0400

    Add files via upload

---
+++ Java popup/virus.java
@@ -0,0 +1,10 @@
+import java.lang.String;
+import javax.swing.JOptionPane;
+
+public class java_virus {
+        public static void main (String args[]) {
+        while (true) {
+            JOptionPane.showConfirmDialog(null,"Virus ","ERROR", JOptionPane.YES_NO_OPTION);
+        }
+    }
+}
