2e5a280eb02a81bc8368d61ff26f2b209c274f6a
IneshV <123998765+IneshV@users.noreply.github.com>
2023-05-04 20:52:20 -0400

    Update 08_virus_string_classifier.py

--- 08_virus_string_classifier.py
+++ 08_virus_string_classifier.py
@@ -4,7 +4,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
-from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix, classification_report
 
 # Define the folders for safe and malicious strings
 safe_strings_folder = 'path/to/safe_strings_folder'
@@ -30,10 +30,7 @@ df = pd.concat([safe_df, malicious_df])
 # Convert the text data into numerical features using the CountVectorizer
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(df['text'])
-
-# Split the data into training and testing sets
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, df['label'], test_size=0.3)
+y = df['label']
 
 # Train multiple models on the data
 models = {
@@ -42,7 +39,25 @@ models = {
     'SVC': SVC()
 }
 for name, model in models.items():
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    model.fit(X, y)
+
+# Load the new file to classify
+new_file_path = 'path/to/new/file'
+with open(new_file_path, 'r') as file:
+    new_file_text = file.read()
+
+# Convert the new file text into numerical features using the same vectorizer used in training
+new_file_X = vectorizer.transform([new_file_text])
+
+# Use the trained models to classify the new file
+for name, model in models.items():
     print(f"Model: {name}")
-    print(classification_report(y_test, y_pred))
+    prediction = model.predict(new_file_X)[0]
+    if prediction == 0:
+        print("Prediction: Safe")
+    else:
+        print("Prediction: Malicious")
+    print("Confusion Matrix:")
+    print(confusion_matrix(y, model.predict(X)))
+    print("Classification Report:")
+    print(classification_report(y, model.predict(X)))
