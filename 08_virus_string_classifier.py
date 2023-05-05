7868e2bd8afbef63804b1e0408a89f592913f293
IneshV <123998765+IneshV@users.noreply.github.com>
2023-05-04 20:46:09 -0400

    Update 08_virus_string_classifier.py

--- 08_virus_string_classifier.py
+++ 08_virus_string_classifier.py
@@ -1,43 +1,48 @@
+import pandas as pd
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
 
-# define the paths to the training data folders
-normal_folder = 'safe_strings_folder'
-virus_folder = 'malicious_string_folder'
+# Define the folders for safe and malicious strings
+safe_strings_folder = 'path/to/safe_strings_folder'
+malicious_strings_folder = 'path/to/malicious_strings_folder'
 
-# define the vectorizer and classifier
+# Load the safe strings into a pandas dataframe
+safe_strings = []
+for filename in os.listdir(safe_strings_folder):
+    with open(os.path.join(safe_strings_folder, filename), 'r') as file:
+        safe_strings.append(file.read())
+safe_df = pd.DataFrame({'text': safe_strings, 'label': 0})
+
+# Load the malicious strings into a pandas dataframe
+malicious_strings = []
+for filename in os.listdir(malicious_strings_folder):
+    with open(os.path.join(malicious_strings_folder, filename), 'r') as file:
+        malicious_strings.append(file.read())
+malicious_df = pd.DataFrame({'text': malicious_strings, 'label': 1})
+
+# Concatenate the safe and malicious dataframes into one dataframe
+df = pd.concat([safe_df, malicious_df])
+
+# Convert the text data into numerical features using the CountVectorizer
 vectorizer = CountVectorizer()
-classifier = MultinomialNB()
+X = vectorizer.fit_transform(df['text'])
 
-# define the training data and labels
-training_data = []
-labels = []
+# Split the data into training and testing sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, df['label'], test_size=0.3)
 
-# iterate through the normal folder and add the data to the training set
-for filename in os.listdir(normal_folder):
-    filepath = os.path.join(normal_folder, filename)
-    with open(filepath, 'r') as f:
-        data = f.read()
-        training_data.append(data)
-        labels.append(0)
-
-# iterate through the virus folder and add the data to the training set
-for filename in os.listdir(virus_folder):
-    filepath = os.path.join(virus_folder, filename)
-    with open(filepath, 'r') as f:
-        data = f.read()
-        training_data.append(data)
-        labels.append(1)
-
-# transform the training data into feature vectors
-features = vectorizer.fit_transform(training_data)
-
-# train the classifier on the feature vectors and labels
-classifier.fit(features, labels)
-
-# test the classifier on some new data
-test_data = ['This is a normal DMG file', 'This is a virus DMG file']
-test_features = vectorizer.transform(test_data)
-predictions = classifier.predict(test_features)
-print(predictions)
+# Train multiple models on the data
+models = {
+    'MultinomialNB': MultinomialNB(),
+    'LogisticRegression': LogisticRegression(),
+    'SVC': SVC()
+}
+for name, model in models.items():
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    print(f"Model: {name}")
+    print(classification_report(y_test, y_pred))
